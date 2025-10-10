9cd134cb3c83278ed2cf8f889c5cbcc7cfae263c
Jeycharan <jeycharan.prabu17@gmail.com>
2025-10-10 15:29:02 +0530

    day 3 tasks

---
+++ script.js
@@ -0,0 +1,24 @@
+// let a=6;
+// let b=a;
+// a=4;
+// console.log(b);
+
+// let obj1={ahe:33};
+// let obj2=obj1;
+// obj1.age=22;
+// console.log(obj2.age);
+
+// let a=true;
+// let b=false;
+// if(a||b){
+//     console.log("executed");
+// }
+console.log(sum(10,23));
+
+function sum(a,b){
+    return a+b;
+}
+const sum2=(a,b,c)=>{
+    console.log(a+b+c);
+}
+sum2(11,22,33);
